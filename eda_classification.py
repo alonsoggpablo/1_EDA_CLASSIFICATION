90bdbce85c3db3a15d8a783cb5a7327e8e9aad76
alonsogpablo@uniovi.es <Pablo15021974>
2020-03-15 16:19:36 +0100

    Hyperparameter tuning

--- eda_classification.py
+++ eda_classification.py
@@ -12,6 +12,7 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 
 dataset=pd.read_csv('adult.data.csv')
 
@@ -32,10 +33,9 @@ b.index.name='index'
 categorical_dataset.index.name='index'
 relevant_dataset=pd.merge(categorical_dataset,b,on='index')
 
-# Check correlations
+# Check correlations to find relevant features
 
 relevant_dataset_corr=relevant_dataset.corr()
-
 
 X=relevant_dataset.drop(columns=['salary'])
 sc=StandardScaler()
@@ -55,9 +55,9 @@ X_onehotencoded=enc.transform(X).toarray()
 
 X_train,X_test,Y_train,Y_test=train_test_split(X_onehotencoded,Y,test_size=0.2)
 
-# Get the PCs
+# PCA
 
-pca=PCA(n_components=6)
+pca=PCA(n_components=4)
 X_train=pca.fit_transform(X_train)
 X_test=pca.transform(X_test)
 
@@ -65,14 +65,32 @@ X_test=pca.transform(X_test)
 # Build and train the model
 #classifier=LogisticRegression()
 #classifier=RandomForestClassifier(max_depth=2,random_state=0)
-#classifier=DecisionTreeClassifier(max_depth=5)
-classifier=GaussianNB()
+#classifier=GaussianNB()
 #classifier=KNeighborsClassifier(3)
 #classifier=SVC()
+
+# Hyperparameters tuning / Decission Tree
+parameters={'max_depth':[2,4,5,7,8,10]}
+grid_search=GridSearchCV(DecisionTreeClassifier(),parameters,cv=3,return_train_score=True)
+grid_search.fit(X_train,Y_train)
+max_depth=grid_search.best_params_['max_depth']
+
+# Hyperparameters tuning / Logistic Regression
+#parameters={'penalty':['l1','l2'],'C':[0.1,0.4,0.8,1,2,5]}
+#grid_search=GridSearchCV(LogisticRegression(solver='liblinear'),parameters,cv=3,return_train_score=True)
+#grid_search.fit(X_train,Y_train)
+#penalty=grid_search.best_params_['penalty']
+#C=grid_search.best_params_['C']
+
+
+#Build the model
+classifier=DecisionTreeClassifier(max_depth=max_depth)
+#classifier=LogisticRegression(penalty=penalty,C=C,solver='liblinear')
+
 classifier.fit(X_train,Y_train)
 Y_pred=classifier.predict(X_test)
 
-# Check accuracy
+# Check accuracy (best classification metric)
 cm=confusion_matrix(Y_test,Y_pred)
 
 target_names=['<=50K','>50K']
